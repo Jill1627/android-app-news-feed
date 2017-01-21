97741ad827c702a460c930141107aef78d6d9523
Jill Gao <jill.h.gao@gmail.com>
2017-01-21 13:14:11 -0500

    review order on second screen

--- app/src/main/java/com/example/android/just_java/Main2Activity.java
+++ app/src/main/java/com/example/android/just_java/Main2Activity.java
@@ -1,7 +1,8 @@
 package com.example.android.just_java;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,4 +11,18 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+    /**
+     * This method is called when the confirm button is clicked.
+     */
+    public void confirmOrder(View view) {
+//        popUpWindow();
+    }
+    /**
+     * This method is called when the confirm button is clicked.
+     */
+
+    public void goBack(View view) {
+
+    }
+
 }
