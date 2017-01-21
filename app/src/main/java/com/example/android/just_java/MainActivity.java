591cf8a743dfd2fa43bad104b9507c4ff54fca6e
Jill Gao <jill.h.gao@gmail.com>
2017-01-21 12:28:11 -0500

    local change in MainActivity

--- app/src/main/java/com/example/android/just_java/MainActivity.java
+++ app/src/main/java/com/example/android/just_java/MainActivity.java
@@ -6,12 +6,13 @@
  */
 package com.example.android.just_java;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
 import java.text.NumberFormat;
-import android.content.Intent;
 
 /**
  * This app displays an order form to order coffee.
